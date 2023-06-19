88a5a6ec9a7df36e32fbd6a380b8792926e17448
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-06-19 13:04:41 -0400

    added product objects

---
+++ assets/scripts/app.js
@@ -0,0 +1,15 @@
+const products = [
+    {
+        title: 'A Pillow',
+        imageURL: 'http://tiny.cc/en48vz',
+        price: 19.99,
+        description: 'A Soft Pillow'
+    },
+    {
+        title: 'A Rug',
+        imageURL: 'http://tiny.cc/co48vz',
+        price: 89.99,
+        description: 'We Buid To Your Design'
+    }
+]
+
